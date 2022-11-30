aee2c8721ab257336db3ccebac9a2408fbb11e21
Archit Garg <72217070+garchit@users.noreply.github.com>
2022-11-30 22:00:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+def main():
+  
+
+
+  st.title("Even or Odd")
+  a=st.number_input("Enter Number")
+  
+
+  if(a%2==0):
+    st.write("Num is EVEN")
+  else:
+    st.write("Num is ODD")
+  
+
+  
+if _name=='__main_':
+  main()
