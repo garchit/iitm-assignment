8552997298ad65c067ae451651a7d00ad7e5b88f
Archit Garg <72217070+garchit@users.noreply.github.com>
2022-11-30 22:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
+# import numpy as np
+# import pandas as pd
+# import streamlit as st
+
+
+# def main():
+  
+
+
+#   st.title("Even or Odd")
+#   a=st.number_input("Enter Number")
+  
+
+#   if(a%2==0):
+#     st.write("Num is EVEN")
+#   else:
+#     st.write("Num is ODD")
+  
+
+  
+# if __name__=='__main__':
+#   main()
 import numpy as np
 import pandas as pd
 import streamlit as st
 
 
 def main():
+  st.title("Multiplication of Two Numbers")
+  html_temp = """
+  <div style="background-color:black;padding:10px">
+  <h2 style="color:black;text-align:center;">Division of 2 Numbers</h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  num1 = st.number_input("Number 1")
+  num2 = st.number_input("Number 2")
   
-
-
-  st.title("Even or Odd")
-  a=st.number_input("Enter Number")
-  
-
-  if(a%2==0):
-    st.write("Num is EVEN")
+  if(num1==0 or num2==0):
+    result = 0
   else:
-    st.write("Num is ODD")
+    result=num1*num2
   
-
+  st.success('The output is {}'.format(result))
   
-if __name__=='__main__':
+if _name=='__main_':
   main()
